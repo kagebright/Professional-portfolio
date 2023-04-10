c99bb0ee8ff0e7ebe069f94f2e886afb4bba96a0
Bright <113262558+kagebright@users.noreply.github.com>
2023-04-09 23:28:37 -0400

    initial commit

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './index.scss'
+import LogoA
+
+const Sidebar = () => {
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={}
+        </Link>
+    </div>
+}
+
+export default Sidebar;
